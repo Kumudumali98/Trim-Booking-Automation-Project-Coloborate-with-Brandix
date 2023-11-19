d51d7fb86b6e95beef4afe8b3e12c466f6aecd86
Kumudumali Sandaleka <kumdudumalij10@gmail.com>
2023-11-19 22:39:48 +0530

    Plm data calculated - some values are different double check

--- Code/TrimBooking/server.R
+++ Code/TrimBooking/server.R
@@ -13,7 +13,7 @@ shinyServer(function(input, output, session) {
     
     
     # read all the uploaded files
-    all_files <- reactive({
+    destinationQty_files <- reactive({
         req(input$upload_invoice)
         purrr::map(input$upload_invoice$datapath, read_excel) %>%
             purrr::set_names(input$upload_invoice$name)
@@ -23,14 +23,34 @@ shinyServer(function(input, output, session) {
     output$selected_invoice_table <- renderTable({
         req(input$invoice_files_rows_selected)
         
-        all_files()[[input$invoice_files_rows_selected]]
+        destinationQty_files()[[input$invoice_files_rows_selected]]
+    })
+    
+    #########################
+    output$plm_output_file <- DT::renderDT({
+        DT::datatable(plm_file(), selection = "single")
+    })
+    
+    # read the uploaded file
+    plm_file <- reactive({
+        req(input$upload_plm_data)
+        read_excel(input$upload_plm_data$datapath)
+    })
+    
+    # select a row in DT files and display the corresponding table
+    output$selected_plm_table <- renderTable({
+        req(input$plm_output_file_rows_selected)
+        
+        plm_file()[input$plm_output_file_rows_selected, ]
     })
     
     
+    
+    ####################3
     modified_data <- reactiveVal(NULL)
     
-    observeEvent(input$processBtn, {
-        if (!is.null(all_files())) {
+    observeEvent(input$process_order, {
+        if (!is.null(destinationQty_files())) {
             # Your existing code
             counter = 0
             size_cols <- c("UNITS", "3XS", "2XS", "XS", "S", "M", "L", "XL", "2XL")
@@ -39,9 +59,9 @@ shinyServer(function(input, output, session) {
             processed_data_list <- list()
             
             # Loop through all the uploaded files
-            for (file_index in seq_along(all_files())) {
+            for (file_index in seq_along(destinationQty_files())) {
                 # Extract a specific data frame from the list
-                quentity <- all_files()[[file_index]]
+                quentity <- destinationQty_files()[[file_index]]
                 
                 # Replace NA values with 0 in specified columns
                 quentity <- quentity %>%
@@ -61,16 +81,20 @@ shinyServer(function(input, output, session) {
                     counter = 1
                 }
                 
-                Total_Order_Summary[5] <- Total_Order_Summary[5] + quentity[5]
+                #Total_Order_Summary[5] <- Total_Order_Summary[5] + quentity[5]
                 
                 # Perform calculations and update the new data frame
                 for (i in 6:13) {
                     Order_Summary[size_cols[i - 4]] <- ceiling(quentity[[i]] * quentity[[5]])
-                    Total_Order_Summary[i] <- ceiling((Total_Order_Summary[i] + Order_Summary[i]) * 1.02)
+                    Total_Order_Summary[i] <- (Total_Order_Summary[i] + Order_Summary[i])
                 }
-                
-                # Save the modified data to the list
-                # processed_data_list[[file_index]] <- Order_Summary
+             
+                 # Save the modified data to the list
+                 processed_data_list[[file_index]] <- Order_Summary
+            }
+            
+            for (i in 6:13) {
+                Total_Order_Summary[i] <- ceiling(Total_Order_Summary[i] * 1.02)
             }
             
             # Combine all processed data frames into a single data frame
@@ -82,24 +106,86 @@ shinyServer(function(input, output, session) {
     })
     
     
-    output$result_files <- DT::renderDT({
+    output$totalOrderQty <- DT::renderDT({
         DT::datatable(modified_data(), selection = "single")
     })
     
     
     
     #select a row in DT files and display the corresponding table
-    output$selected_prosessed_table <- renderTable({
-        req(input$result_files_rows_selected)
+    output$total_order_qty <- renderTable({
+        req(input$totalOrderQty_rows_selected)
         
-        all_files()[[input$result_files_rows_selected]]
+        destinationQty_files()[[input$totalOrderQty_rows_selected]]
     })
     
-    output$dl <- downloadHandler(
+    output$dlTotal_Order_Qty <- downloadHandler(
         filename = function() {"Order_Summary.xlsx"},
         content = function(excel_file) {
             write.xlsx(modified_data(), excel_file, rowNames = FALSE)}
     )
+    
+    ###################################################3
+    
+    TotalRM_data <- reactiveVal(NULL)
+    
+    observeEvent(input$process_RMQty, {
+        if (!is.null(plm_file())) {
+            
+            plm_data <- plm_file()
+            plm_data <- plm_data %>% mutate(Style_Color = toupper(plm_data$`Color`), .keep = "unused")
+            
+            Total_Order_Summary <-  modified_data() %>% rename(Style_Color = Colors)
+            
+            
+            merged_df <- merge(x = plm_data, y = Total_Order_Summary,
+                               by = c("Style Number", "Style_Color"),
+                               all.x = T)
+            
+            result <- merged_df
+            
+            result[12:19] <- 0
+            
+            counter = 12
+            for (i in 12:19) {
+                result[counter] <- ceiling(merged_df[i] * merged_df$Consumption * (1+merged_df$Wastage))
+                counter = counter + 1
+            }
+            
+            result <- result %>% rename(Style_Number = `Style Number`)
+            result <- result[result$Consumption != 0 | result$`RM Color` != "N/A",]
+            
+            
+            result <- result %>%
+                group_by(`RM Reference`, `RM Color`) %>%
+                summarise(across(c(`3XS`, `2XS`, `XS`, `S`, `M`, `L`, `XL`, `2XL`), sum))
+            
+            
+            # Save the combined data
+            TotalRM_data(result)
+        }
+    })
+    
+    
+    output$tRMQty_file <- DT::renderDT({
+        DT::datatable(TotalRM_data(), selection = "single")
+    })
+    
+    
+    
+    #select a row in DT files and display the corresponding table
+    output$total_RM_qty <- renderTable({
+        req(input$tRMQty_file_rows_selected)
+        
+        plm_file()[[input$tRMQty_file_rows_selected]]
+    })
+    
+    output$dltotal_RM_qty <- downloadHandler(
+        filename = function() {"Total_RM_Qty.xlsx"},
+        content = function(excel_file) {
+            write.xlsx(TotalRM_data(), excel_file, rowNames = FALSE)}
+    )
+    
     
 })
 
--- Code/TrimBooking/ui.R
+++ Code/TrimBooking/ui.R
@@ -12,7 +12,8 @@ shinyUI(dashboardPage(
         sidebarMenu(
             menuItem("Upload Invoice", tabName = "invoice", icon = icon("cloud-upload")),
             menuItem("Data", tabName = "data", icon = icon("info")),
-            menuItem("Results", tabName = "results", icon = icon("info")),
+            menuItem("Total Garment Order", tabName = "totalGarmentOrder", icon = icon("info")),
+            menuItem("Total Raw Material Quantity", tabName = "totalRMQty", icon = icon("info")),
             menuItem("Comparison", tabName = "comparison", icon = icon("info"))
         )
     ),
@@ -23,19 +24,31 @@ shinyUI(dashboardPage(
                 h2("Upload Invoice"),
                 textInput("invoiceNumber", "Enter Invoice Number"),
                 fileInput("upload_invoice", NULL, buttonLabel = "Upload Invoice...", multiple = TRUE),
-                actionButton("processBtn", "Process Invoice")
+                actionButton("process_order", "Process Order"),
+                fileInput("upload_plm_data", NULL, buttonLabel = "Upload PLM B...", multiple = FALSE),
+                actionButton("process_RMQty", "Process Raw Material Quantity")
             ),
             tabItem(
                 tabName = "data",
                 DT::DTOutput("invoice_files"),
-                tableOutput("selected_invoice_table")
+                tableOutput("selected_invoice_table"),
+                
+                DT::DTOutput("plm_output_file"),
+                tableOutput("selected_plm_table")
             ),
             tabItem(
-                tabName = "results",
-                DT::DTOutput("result_files"),
-                tableOutput("selected_prosessed_table"),
+                tabName = "totalGarmentOrder",
+                DT::DTOutput("totalOrderQty"),
+                tableOutput("total_order_qty"),
                 
-                downloadButton("dl", "Download")
+                downloadButton("dlTotal_Order_Qty", "Download")
+            ),
+            tabItem(
+                tabName = "totalRMQty",
+                DT::DTOutput("tRMQty_file"),
+                tableOutput("total_RM_qty"),
+                
+                downloadButton("dlTotal_RM_Qty", "Download")
             ),
             tabItem(
                 tabName = "comparison"
