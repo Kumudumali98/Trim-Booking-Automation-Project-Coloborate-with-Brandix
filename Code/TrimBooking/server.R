78ec9cf2da47b4f51e98288a7bb1e66b6075bc5a
Kumudumali Sandaleka <kumdudumalij10@gmail.com>
2023-12-12 10:10:42 +0530

    Commented

--- Code/TrimBooking/server.R
+++ Code/TrimBooking/server.R
@@ -8,19 +8,18 @@ library(janitor)
 
 shinyServer(function(input, output, session) {
     
+    # Render the Brandix logo image
     output$home_img <- renderImage({
-        
         list(id = "logoImage", src = "../../www/Logo.png",height = "183px", width="275px" )
-        
     }, deleteFile = F)
     
-    
+    # Render the datatable for uploaded invoice files
     output$invoice_files <- DT::renderDT({
         DT::datatable(input$upload_invoice, selection = "single")
     })
     
     
-    # read all the uploaded files
+    # read all the uploaded invoice files
     destinationQty_files <- reactive({
         req(input$upload_invoice)
         purrr::map(input$upload_invoice$datapath, read_excel) %>%
@@ -30,16 +29,15 @@ shinyServer(function(input, output, session) {
     #select a row in DT files and display the corresponding table
     output$selected_invoice_table <- renderTable({
         req(input$invoice_files_rows_selected)
-        
         destinationQty_files()[[input$invoice_files_rows_selected]]
     })
     
-    #########################
+    # Render the datatable for uploaded PLM BOM file
     output$plm_output_file <- DT::renderDT({
         DT::datatable(plm_file(), selection = "single")
     })
     
-    # read the uploaded file
+    # read the uploaded PLM BOM file
     plm_file <- reactive({
         req(input$upload_plm_data)
         read_excel(input$upload_plm_data$datapath)
@@ -52,11 +50,10 @@ shinyServer(function(input, output, session) {
         plm_file()[input$plm_output_file_rows_selected, ]
     })
     
-    
-    
-    ####################3
+    # Reactive value for total order summary with buffer
     total_Order_w_buffer_data <- reactiveVal(NULL)
     
+    # Observe event for processing order data
     observeEvent(input$process_order, {
         if (!is.null(destinationQty_files())) {
             # Your existing code
@@ -94,11 +91,12 @@ shinyServer(function(input, output, session) {
                     Order_Summary[size_cols[i - 2]] <- ceiling(quentity[[i]] * quentity[[3]])
                     Total_Order_Summary[i] <- (Total_Order_Summary[i] + Order_Summary[i])
                 }
-             
-                 # Save the modified data to the list
-                 processed_data_list[[file_index]] <- Order_Summary
+                
+                # Save the modified data to the list
+                processed_data_list[[file_index]] <- Order_Summary
             }
             
+            # Apply buffer to total order summary
             for (i in 4:11) {
                 Total_Order_Summary[i] <- ceiling(Total_Order_Summary[i] * (100+input$buffer)/100)
             }
@@ -109,48 +107,53 @@ shinyServer(function(input, output, session) {
     })
     
     
-    #select a row in DT files and display the corresponding table
+    # Render a table for the total order quantity with buffer
     output$total_order_qty <- renderTable(total_Order_w_buffer_data())
     
+    # Download handler for total order quantity with buffer
     output$dlTotal_Order_Qty_w_buffer <- downloadHandler(
         filename = function() {"Order_Summary_with_buffer.xlsx"},
         content = function(excel_file) {
             write.xlsx(total_Order_w_buffer_data(), excel_file, rowNames = FALSE)}
     )
-
-    ###################################################3
     
+    # Reactive value for total raw material data
     TotalRM_data <- reactiveVal(NULL)
     
+    # Observe event for processing raw material quantity data
     observeEvent(input$process_RMQty, {
         if (!is.null(plm_file())) {
             
+            # Read and preprocess PLM data
             plm_data <- plm_file()
             plm_data <- plm_data %>% mutate(Style_Color = toupper(plm_data$`Color`), .keep = "unused")
             
+            # Retrieve total order summary with buffer
             Total_Order_Summary <-  total_Order_w_buffer_data() 
             Total_Order_Summary <- Total_Order_Summary %>% 
                 mutate(Style_Color = toupper(Total_Order_Summary$`Colour`), .keep = "unused")
             
+            # Merge PLM data with total order summary
             merged_df <- merge(x = plm_data, y = Total_Order_Summary,
                                by = c("Style Number", "Style_Color"),
                                all.x = T)
             
+            # Initialize result data frame
             result <- merged_df
-            
-            
             result[11:18] <- 0
             
+            # Calculate raw material quantities
             counter = 11
             for (i in 11:18) {
                 result[counter] <- ceiling(merged_df[i] * merged_df$Consumption * (1+merged_df$Wastage))
                 counter = counter + 1
             }
             
+            # Rename columns
             result <- result %>% rename(Style_Number = `Style Number`)
             result <- result[result$Consumption != 0 | result$`RM Color` != "N/A",]
             
-            
+            # Filter out unnecessary rows
             result <- result %>%
                 group_by(`RM Reference`, `RM Color`) %>%
                 summarise(across(c(`3XS`, `2XS`, `XS`, `S`, `M`, `L`, `XL`, `2XL`), sum))
@@ -163,10 +166,10 @@ shinyServer(function(input, output, session) {
     })
     
     
-    #select a row in DT files and display the corresponding table
+    # Render a table for the total raw material quantity
     output$total_RM_qty <- renderTable(TotalRM_data())
     
-    
+    # Download handler for total raw material quantity
     output$dlTotal_RM_Qty <- downloadHandler(
         filename = function() {"Total_RM_Qty.xlsx"},
         content = function(excel_file) {
@@ -175,8 +178,7 @@ shinyServer(function(input, output, session) {
     ) 
     
     
-    ###############################################################################################    
-    # read the uploaded file
+    # Reactive values for before and after confirmed files
     before_confirmed <- reactive({
         req(input$upload_file1)
         read_excel(input$upload_file1$datapath)
@@ -187,14 +189,18 @@ shinyServer(function(input, output, session) {
         read_excel(input$upload_file2$datapath)
     })
     
+    #Reactive value for storing comparison data
     Comparison_data <- reactiveVal(NULL)
     
+    # Observe event for processing comparison data
     observeEvent(input$process_comparison, {
         if (!is.null(before_confirmed()) && !is.null(after_confirmed())) {
             
+            # Extract data from before and after confirmed files
             before_con <- before_confirmed()
             after_con <- after_confirmed()
             
+            # Select relevant columns for comparison
             comparison <- before_con %>% select(`RM Reference`, `RM Color`)
             
             # Calculate the differences for retail prices and quantities
@@ -209,9 +215,10 @@ shinyServer(function(input, output, session) {
     })
     
     
-    #select a row in DT files and display the corresponding table
+    # Render a table for the comparison data
     output$comparison <- renderTable(Comparison_data())
     
+    # Download handler for the comparison data
     output$dl_comparison <- downloadHandler(
         filename = function() {"Comparison.xlsx"},
         content = function(excel_file) {
--- Code/TrimBooking/ui.R
+++ Code/TrimBooking/ui.R
@@ -1,3 +1,4 @@
+# Load necessary libraries
 library(shiny)
 library(shinydashboard)
 library(DT)
@@ -6,12 +7,14 @@ library(openxlsx)
 library(dplyr)
 library(janitor)
 
+# Shiny UI definition
 shinyUI(dashboardPage(
     skin = "red",
-    dashboardHeader(title = "Brandix - Trim Booking Automation",titleWidth = 350),
+    dashboardHeader(title = "Brandix - Trim Booking Automation", titleWidth = 350),
     dashboardSidebar(
         width = 350,
         
+        # Sidebar menu
         sidebarMenu(
             menuItem("Home", tabName = "home", icon = icon("home", style = "padding-right:10px;")),
             menuItem("Upload Invoice", tabName = "invoice", icon = icon("cloud-upload", style = "padding-right:10px;")),
@@ -22,70 +25,106 @@ shinyUI(dashboardPage(
         )
     ),
     dashboardBody(
-            tabItems(
-                tabItem(
-                    tabName = "home", 
-                    h3(strong("Welcome To Trim Booking Automation")),
-                    hr(),
-                    p(style="text-align: justify; font-size : 16px;",
-                      "This online application was designed to automate the trim 
-                      booking process for Brandix by focusing on critical 
-                      calculations such as total garment quantity, total 
-                      trim quantity at the trim booking and order confirmation 
-                      phases, and comparative trim quantity data analysis. Go to",
-                      a(href = "https://github.com/Kumudumali98/MT-325-Trim-Booking-Group.git",
-                        "Trim Bookning Automation GitHub Page"),
-                      "to find more details on the source code."),
-                    hr(),
-                    tags$blockquote("Shiny-Box is still under continuous development. Please look forward to future updates!"),
-                    hr(),
-                    tags$div(
-                        style = "text-align: center;",  
-                        imageOutput("home_img")
-                    ),
+        tabItems(
+            tabItem(
+                tabName = "home", 
+                h3(strong("Welcome To Trim Booking Automation")),
+                hr(),
+                p(
+                    style = "text-align: justify; font-size : 16px;",
+                    "This online application was designed to automate the trim booking process for Brandix by focusing on critical calculations such as total garment quantity, total trim quantity at the trim booking and order confirmation phases, and comparative trim quantity data analysis. Go to",
+                    a(href = "https://github.com/Kumudumali98/MT-325-Trim-Booking-Group.git",
+                      "Trim Booking Automation GitHub Page"),
+                    "to find more details on the source code."
                 ),
-                tabItem(
-                    tabName = "invoice", 
-                    h3("Upload Invoice"),
-                    textInput("invoiceNumber", "Enter Invoice Number"),
-                    fileInput("upload_invoice", NULL, buttonLabel = "Upload Perchase Order", multiple = TRUE),
-                    h3("Enter Buffer"),
-                    numericInput("buffer", "Enter Buffer value (%):", 2, min = 1, max = 100),
-                    actionButton("process_order", "Process Order"),
-                    h3("Upload PLM files", style="margin-bottom:25px;"),
-                    fileInput("upload_plm_data", NULL, buttonLabel = "Upload PLM BOM", multiple = FALSE),
-                    actionButton("process_RMQty", "Process Raw Material Quantity")
+                hr(),
+                tags$blockquote("To be THE inspired solution for branded clothing."),
+                hr(),
+                tags$div(
+                    style = "text-align: center;",  
+                    imageOutput("home_img")
                 ),
-                tabItem(
-                    tabName = "data",
-                    DT::DTOutput("invoice_files"),
-                    tableOutput("selected_invoice_table"),
-                    DT::DTOutput("plm_output_file"),
-                    tableOutput("selected_plm_table")
-                ),
-                tabItem(
-                    tabName = "totalGarmentOrder",
-                    tableOutput("total_order_qty"),
-                    downloadButton("dlTotal_Order_Qty_w_buffer", "Download")
-                ),
-                tabItem(
-                    tabName = "totalRMQty",
-                    tableOutput("total_RM_qty"),
-                    
-                    downloadButton("dlTotal_RM_Qty", "Download")
-                ),
-                tabItem(
-                    tabName = "comparison",
-                    h3("Enter Trim Booking Raw Material Order File"),
-                    fileInput("upload_file1", NULL, buttonLabel = "Upload", multiple = FALSE),
-                    h3("Enter  Trim Booking Raw Material Confirmed Order File"),
-                    fileInput("upload_file2", NULL, buttonLabel = "Upload", multiple = FALSE),
-                    actionButton("process_comparison", "Comparison"),
-                    tableOutput("comparison"),
-                    downloadButton("dl_comparison", "Download")
-                )
+            ),
+            tabItem(
+                tabName = "invoice", 
+                h3("Upload Invoice"),
+                
+                # Input for entering invoice number
+                textInput("invoiceNumber", "Enter Invoice Number"),
+                
+                # File input for uploading purchase order
+                fileInput("upload_invoice", NULL, buttonLabel = "Upload Perchase Order", multiple = TRUE),
+                
+                h3("Enter Buffer"),
+                
+                # Numeric input for buffer value
+                numericInput("buffer", "Enter Buffer value (%):", 2, min = 1, max = 100),
+                
+                # Action button to trigger order processing
+                actionButton("process_order", "Process Order"),
+                
+                h3("Upload PLM files", style="margin-bottom:25px;"),
+                
+                # File input for uploading PLM BOM
+                fileInput("upload_plm_data", NULL, buttonLabel = "Upload PLM BOM", multiple = FALSE),
+                
+                # Action button to trigger raw material quantity processing
+                actionButton("process_RMQty", "Process Raw Material Quantity")
+            ),
+            tabItem(
+                tabName = "data",
+                
+                # Output for displaying uploaded invoice files
+                DT::DTOutput("invoice_files"),
+                
+                # Output for displaying selected invoice table
+                tableOutput("selected_invoice_table"),
+                
+                # Output for displaying uploaded PLM BOM files
+                DT::DTOutput("plm_output_file"),
+                
+                # Output for displaying selected PLM table
+                tableOutput("selected_plm_table")
+            ),
+            tabItem(
+                tabName = "totalGarmentOrder",
+                
+                # Output for displaying total garment order quantity
+                tableOutput("total_order_qty"),
+                
+                # Download button for downloading total garment order quantity
+                downloadButton("dlTotal_Order_Qty_w_buffer", "Download")
+            ),
+            tabItem(
+                tabName = "totalRMQty",
+                
+                # Output for displaying total raw material quantity
+                tableOutput("total_RM_qty"),
+                
+                # Download button for downloading total raw material quantity
+                downloadButton("dlTotal_RM_Qty", "Download")
+            ),
+            tabItem(
+                tabName = "comparison",
+                h3("Enter Trim Booking Raw Material Order File"),
+                
+                # File input for uploading raw material order file
+                fileInput("upload_file1", NULL, buttonLabel = "Upload", multiple = FALSE),
+                
+                h3("Enter  Trim Booking Raw Material Confirmed Order File"),
+                
+                # File input for uploading confirmed raw material order file
+                fileInput("upload_file2", NULL, buttonLabel = "Upload", multiple = FALSE),
+                
+                # Action button to trigger comparison
+                actionButton("process_comparison", "Comparison"),
+                
+                # Output for displaying comparison results
+                tableOutput("comparison"),
+                
+                # Download button for downloading comparison results
+                downloadButton("dl_comparison", "Download")
+            )
         )
     )
-        
 ))
-    
